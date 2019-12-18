f351012042173e517d44e9b084b6b9e2107afa84
njasi <nick@jasinski3.com>
2019-12-17 20:41:51 -0600

    git auto pushing test setup

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,14 @@ public class Main {
     public static void main(String[] args) {
         ProcessBuilder processBuilder = new ProcessBuilder();
         try {
-            updateNum();
+            pushToGit(updateNum());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void updateNum() throws IOException {
+    private static int updateNum() throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("change.txt"));
         String firstLine = br.readLine(); // eat the first line
         int num = Integer.parseInt(br.readLine());
@@ -19,9 +20,12 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new FileWriter("change.txt"));
         bw.write(String.format("%s\n%d", firstLine, num + 1));
         bw.close();
+        return num + 1;
     }
 
-    private static void pushToGit(){
-
+    private static void pushToGit(int changeNum) throws IOException, InterruptedException {
+        Runtime.getRuntime().exec("git add change.txt").waitFor();
+        Runtime.getRuntime().exec(String.format("git commit -m\"Daily commit #%d\"", changeNum)).waitFor();
+        Runtime.getRuntime().exec("git push").waitFor();
     }
 }
